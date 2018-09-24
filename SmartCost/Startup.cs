c3799b91b1968d1cc91169e96ac63b82fe394c35
Berkay Kırmızıoğlu <berkay.kirmizioglu@sahabt.com>
2018-09-24 22:03:24 +0300

    Add project files.

---
+++ SmartCost/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartCost.Startup))]
+namespace SmartCost
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
